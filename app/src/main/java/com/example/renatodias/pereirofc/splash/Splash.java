8a4082459333830e4b308df36dbfd350bf311c2a
Renato Dias <lrds1722@gmail.com>
2017-07-25 09:12:35 -0300

    add splash do time

---
+++ app/src/main/java/com/example/renatodias/pereirofc/splash/Splash.java
@@ -0,0 +1,36 @@
+package com.example.renatodias.pereirofc.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.renatodias.pereirofc.MainActivity;
+import com.example.renatodias.pereirofc.R;
+
+/**
+ * Created by renatodias on 25/07/17.
+ */
+
+
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
